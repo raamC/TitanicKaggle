652f76d99dfbe5601705737f8389f4a6eb1a7be2
raamC <raamrajchauhan@gmail.com>
2018-03-19 20:32:32 +0000

    Export working CSV file ready for first upload

--- TitanicDecisionTrees.py
+++ TitanicDecisionTrees.py
@@ -1,54 +1,42 @@
+import numpy as np
 import pandas as pd
 from sklearn import tree
 from sklearn.ensemble import RandomForestClassifier
-
-
-# ----- FUNCTIONS -----
 
 def cleanData(dataframe):
     dataframe['Sex'] = dataframe['Sex'].map({'male': 0, 'female': 1})
     dataframe['Embarked'] = dataframe['Embarked'].map({'C': 0, 'Q': 1, 'S': 2})
     return dataframe
 
+def addMissingAges(dataFrame, ageMean, ageSD):
+    dataFrame['Age'] = np.where(np.isnan(dataFrame['Age']), np.random.normal(ageMean, ageSD, 1)[0], dataFrame['Age'])
+    return dataFrame
 
+def createResultsDF(dataframe, classifier, _features):
+    ids = dataframe['PassengerId']
+    results = pd.DataFrame(classifier.predict(dataframe[_features]), columns=['Survived'])
+    return pd.concat([ids,results], axis=1, join='inner')
 
-# ----- TRAINING -----
 trainingDF = pd.read_csv('./train.csv', header = 0)
-cleanData(trainingDF)
+trainingAgeMean = np.nanmean(list(trainingDF['Age']))
+trainingAgeSD = np.nanstd(list(trainingDF['Age']))
 
-features = list(trainingDF.columns[[2,4,5,6,7,9,11]])
+cleanData(trainingDF)
+addMissingAges(trainingDF, trainingAgeMean, trainingAgeSD)
+
+# TODO also consider Fare and Embarked column
+# features = list(trainingDF.columns[[2,4,5,6,7,9,11]])
+features = list(trainingDF.columns[[2,4,5,6,7]])
 x = trainingDF[features]
 y = trainingDF['Survived']
 
-
-
-
-# ----- Random Forest -----
-# This specifies the number of trees to use
 clf = RandomForestClassifier(n_estimators=10)
 clf = clf.fit(x, y)
 
-# # print (clf.predict([[3, 1, 15, 1, 0, 5,0]]))
-
-# # ----- TESTING -----
 testDF= pd.read_csv('./test.csv', header = 0)
 cleanData(testDF)
+addMissingAges(testDF, trainingAgeMean, trainingAgeSD)
 
-ids = testDF['PassengerId'][:3]
-results = pd.DataFrame(clf.predict(testDF[features][:3]), columns=['Survived'])
-
-resultsDF = pd.concat([ids,results], axis=1, join='inner')
+resultsDF = createResultsDF(testDF,clf,features)
 
 resultsDF.to_csv('./results.csv', index=False)
-
-print(resultsDF)
-
-
-# TODOs
-
-# Sensibly handle age in training set, rather than
-# deleting it, generate a random number based on
-# the mean and SD.
-
-# Figure out how to pass in an age value for the 
-# test data if the field is empty 
